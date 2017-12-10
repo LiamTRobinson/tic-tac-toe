67fee6117684c172df09121e5dd75ebb3dad63da
LiamTRobinson <Liamtrobinson@gmail.com>
2017-12-10 17:20:07 -0500

    add end game

--- tic-tac-toe/public/javascripts/index.js
+++ tic-tac-toe/public/javascripts/index.js
@@ -1,12 +1,15 @@
 $(function() {
 
-	function GameState(gameBoard, player) {
+	function GameState(gameBoard, player, startingPlayer) {
 		this.gameBoard = gameBoard;
 		this.currentTurn = player;
+		this.startingPlayer = startingPlayer;
 	}
 
 	const CurrentGame = {
-		gameState: new GameState([[0,0,0],[0,0,0],[0,0,0]], 1),
+		gameState: new GameState([[0,0,0],[0,0,0],[0,0,0]], 1, 1),
+		active: false,
+		previousStates: [],
 		updateBoard: function(xPosition, yPosition) {
 			this.gameState.gameBoard[yPosition][xPosition] = this.gameState.currentTurn;
 		},
@@ -18,11 +21,21 @@ $(function() {
 				this.gameState.currentTurn = 1;
 			}
 		},
-		previousStates: []
+		newGame: function() {
+			this.gameState = new GameState([[0,0,0],[0,0,0],[0,0,0]], 1, 1);
+			this.active = true;
+			console.log(this.active);
+			this.previousStates = [];
+		},
+		endGame: function() {
+			this.active = false;
+		}
 	}
 
+	const SavedGames = [];
+
 	const ViewControl = {
-		updateView: function() {
+		updateCells: function() {
 			$(".cell").removeClass("player-1");
 			$(".cell").removeClass("player-2");
 			$(".cell").removeClass("player-0");
@@ -39,6 +52,7 @@ $(function() {
 			CurrentGame.gameState.gameBoard.forEach(function(row) {
 				if (row[0] !== 0 && row[0] === row[1] && row[0] === row[2]) {
 					alert("winner row");
+					CurrentGame.endGame();
 				}
 			});
 		},
@@ -46,6 +60,7 @@ $(function() {
 			CurrentGame.gameState.gameBoard[0].forEach(function(cell, index) {
 				if (cell !== 0 && CurrentGame.gameState.gameBoard[1][index] === cell && CurrentGame.gameState.gameBoard[2][index] === cell) {
 					alert("winner column");
+					CurrentGame.endGame();
 				}
 			});
 		},
@@ -53,22 +68,27 @@ $(function() {
 			let gameBoard = CurrentGame.gameState.gameBoard
 			if (gameBoard[1][1] !== 0) {
 				if ((gameBoard[1][1] === gameBoard[0][0] && gameBoard[1][1] === gameBoard[2][2]) || (gameBoard[1][1] === gameBoard[0][2] && gameBoard[1][1] === gameBoard[2][0])) {
-					alert("winner diagonal")
+					alert("winner diagonal");
+					CurrentGame.endGame();
 				}
 			}
 		}
 	}
 
 	$(".cell").on("click", function() {
-		if ($(this).attr("class").includes("0")) {
+		if ($(this).attr("class").includes("0") && CurrentGame.active === true) {
 			let yPosition = $(this).attr("id").slice(0,1);
 			let xPosition = $(this).attr("id").slice(2);
 			CurrentGame.updateBoard(xPosition, yPosition);
 			CurrentGame.updatePlayer();
-			ViewControl.updateView();
+			ViewControl.updateCells();
 			GameLogic.checkRows();
 			GameLogic.checkColumns();
 			GameLogic.checkDiagonals();
 		}
+	});
+	$("#new-game-button").on("click", function() {
+		CurrentGame.newGame();
+		ViewControl.updateCells();
 	});
 });
